13c8c35d123b09a22114c4f82e880304461d7bd4
Fernanda Vidal <fevidal.dev@gmail.com>
2022-09-23 17:37:23 -0300

    Corrige model User.

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const UserSchema = (sequelize, DataTypes) => {
     const UserTable = sequelize.define('User', {
+        id: DataTypes.INTEGER,
         displayName: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
@@ -9,10 +10,6 @@ const UserSchema = (sequelize, DataTypes) => {
         underscored: true,
         tableName: 'users'
     });
-
-    // UserTable.associate = (models) => {
-    //     UserTable.hasMany()
-    // }
 
     return UserTable;
 };
