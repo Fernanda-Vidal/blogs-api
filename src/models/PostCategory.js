94cb09cbaf1755d5f92ee3727d4a5f9e7b8f5d6b
Fernanda Vidal <fevidal.dev@gmail.com>
2022-09-26 10:29:30 -0300

    Implementa model Post Category.

---
+++ src/models/PostCategory.js
@@ -0,0 +1,39 @@
+const PostCategorySchema = (sequelize, DataTypes) => {
+    const PostCategoryTable = sequelize.define('PostCategory', {
+        postId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            foreignKey: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            foreignKey: true,
+        },
+    },
+    {
+        timestamps: false,
+        underscored: true,
+        tableName: 'postCategories'
+    },
+);
+
+PostCategoryTable.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+        as: 'categories',
+        through: PostCategoryTable,
+        foreignKey: 'postId',
+        otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+        as: 'blogPost',
+        through: PostCategoryTable,
+        foreignKey: 'categoryId',
+        otherKey: 'postId',
+    });
+};
+
+return PostCategoryTable;
+};
+
+module.exports = PostCategorySchema;
